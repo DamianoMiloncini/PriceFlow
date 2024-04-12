edde78a45feab7d28be8afa54e6c6fe1cb6690a6
Damiano Miloncini <2244969@edu.vaniercollege.qc.ca>
2024-04-12 14:49:51 -0400

    Created the entire database

--- projectDB.sql
+++ projectDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 11, 2024 at 04:37 PM
+-- Generation Time: Apr 12, 2024 at 08:44 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -21,13 +21,140 @@ SET time_zone = "+00:00";
 -- Database: `projectDB`
 --
 
+CREATE DATABASE IF NOT EXISTS `projectDB` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `projectDB`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Boomarked_Item`
+--
+
+CREATE TABLE `Boomarked_Item` (
+  `item_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Cart`
+--
+
+CREATE TABLE `Cart` (
+  `cart_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `cart_price` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Item`
+--
+
+CREATE TABLE `Item` (
+  `item_id` int(11) NOT NULL,
+  `name` varchar(35) NOT NULL,
+  `price` float NOT NULL,
+  `image` blob NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `brand` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Items_In_Cart`
+--
+
+CREATE TABLE `Items_In_Cart` (
+  `cart_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `quantity_purchased` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Items_In_Recipe`
+--
+
+CREATE TABLE `Items_In_Recipe` (
+  `recipe_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `quantity_needed` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Item_From_Search_Query`
+--
+
+CREATE TABLE `Item_From_Search_Query` (
+  `item_id` int(11) NOT NULL,
+  `search_query` varchar(75) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Item_Store_Location`
+--
+
+CREATE TABLE `Item_Store_Location` (
+  `item_id` int(11) NOT NULL,
+  `store_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Recipe`
+--
+
+CREATE TABLE `Recipe` (
+  `recipe_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(45) NOT NULL,
+  `content` varchar(500) NOT NULL,
+  `date_created` date NOT NULL,
+  `privacy_status` tinyint(1) NOT NULL DEFAULT 1,
+  `duration` varchar(10) NOT NULL,
+  `image` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Search_Query`
+--
+
+CREATE TABLE `Search_Query` (
+  `search_query` varchar(75) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Store`
+--
+
+CREATE TABLE `Store` (
+  `store_id` int(11) NOT NULL,
+  `store_name` varchar(25) NOT NULL,
+  `address` varchar(25) NOT NULL,
+  `postal_code` varchar(10) NOT NULL,
+  `city` text NOT NULL,
+  `province` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `user`
 --
-CREATE DATABASE IF NOT EXISTS 'projectDB' DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE 'projectDB';
 
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
@@ -39,12 +166,80 @@ CREATE TABLE `user` (
   `street` varchar(25) DEFAULT NULL,
   `postal_code` varchar(15) DEFAULT NULL,
   `city` varchar(20) DEFAULT NULL,
-  `province` varchar(20) DEFAULT NULL
+  `province` varchar(20) DEFAULT NULL,
+  `secret` int(32) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `Boomarked_Item`
+--
+ALTER TABLE `Boomarked_Item`
+  ADD PRIMARY KEY (`item_id`,`user_id`),
+  ADD KEY `user_id_FOREIGN_KEY5` (`user_id`);
+
+--
+-- Indexes for table `Cart`
+--
+ALTER TABLE `Cart`
+  ADD PRIMARY KEY (`cart_id`),
+  ADD KEY `user_id_FOREIGN_KEY` (`user_id`);
+
+--
+-- Indexes for table `Item`
+--
+ALTER TABLE `Item`
+  ADD PRIMARY KEY (`item_id`);
+
+--
+-- Indexes for table `Items_In_Cart`
+--
+ALTER TABLE `Items_In_Cart`
+  ADD PRIMARY KEY (`cart_id`,`item_id`),
+  ADD KEY `item_id_FOREIGN_KEY3` (`item_id`);
+
+--
+-- Indexes for table `Items_In_Recipe`
+--
+ALTER TABLE `Items_In_Recipe`
+  ADD PRIMARY KEY (`recipe_id`,`item_id`),
+  ADD KEY `item_id_FOREIGN_KEY6` (`item_id`);
+
+--
+-- Indexes for table `Item_From_Search_Query`
+--
+ALTER TABLE `Item_From_Search_Query`
+  ADD PRIMARY KEY (`item_id`,`search_query`),
+  ADD KEY `search_query_FOREIGN_KEY` (`search_query`);
+
+--
+-- Indexes for table `Item_Store_Location`
+--
+ALTER TABLE `Item_Store_Location`
+  ADD PRIMARY KEY (`item_id`,`store_id`),
+  ADD KEY `store_id_FOREIGN_KEY2` (`store_id`);
+
+--
+-- Indexes for table `Recipe`
+--
+ALTER TABLE `Recipe`
+  ADD PRIMARY KEY (`recipe_id`),
+  ADD KEY `user_id_FOREIGN_KEY2` (`user_id`);
+
+--
+-- Indexes for table `Search_Query`
+--
+ALTER TABLE `Search_Query`
+  ADD PRIMARY KEY (`search_query`);
+
+--
+-- Indexes for table `Store`
+--
+ALTER TABLE `Store`
+  ADD PRIMARY KEY (`store_id`);
 
 --
 -- Indexes for table `user`
@@ -58,10 +253,86 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `Cart`
+--
+ALTER TABLE `Cart`
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Item`
+--
+ALTER TABLE `Item`
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Recipe`
+--
+ALTER TABLE `Recipe`
+  MODIFY `recipe_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Store`
+--
+ALTER TABLE `Store`
+  MODIFY `store_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Boomarked_Item`
+--
+ALTER TABLE `Boomarked_Item`
+  ADD CONSTRAINT `item_id_FOREIGN_KEY4` FOREIGN KEY (`item_id`) REFERENCES `Item` (`item_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `user_id_FOREIGN_KEY5` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `Cart`
+--
+ALTER TABLE `Cart`
+  ADD CONSTRAINT `user_id_FOREIGN_KEY` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `Items_In_Cart`
+--
+ALTER TABLE `Items_In_Cart`
+  ADD CONSTRAINT `cart_id_FOREIGN_KEY` FOREIGN KEY (`cart_id`) REFERENCES `Cart` (`cart_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `item_id_FOREIGN_KEY3` FOREIGN KEY (`item_id`) REFERENCES `Item` (`item_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `Items_In_Recipe`
+--
+ALTER TABLE `Items_In_Recipe`
+  ADD CONSTRAINT `item_id_FOREIGN_KEY6` FOREIGN KEY (`item_id`) REFERENCES `Item` (`item_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `recipe_id_FOREIGN_KEY` FOREIGN KEY (`recipe_id`) REFERENCES `Recipe` (`recipe_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `Item_From_Search_Query`
+--
+ALTER TABLE `Item_From_Search_Query`
+  ADD CONSTRAINT `item_id_FOREIGN_KEY` FOREIGN KEY (`item_id`) REFERENCES `Item` (`item_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `search_query_FOREIGN_KEY` FOREIGN KEY (`search_query`) REFERENCES `Search_Query` (`search_query`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `Item_Store_Location`
+--
+ALTER TABLE `Item_Store_Location`
+  ADD CONSTRAINT `item_id_FOREIGN_KEY2` FOREIGN KEY (`item_id`) REFERENCES `Item` (`item_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `store_id_FOREIGN_KEY` FOREIGN KEY (`store_id`) REFERENCES `Store` (`store_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `store_id_FOREIGN_KEY2` FOREIGN KEY (`store_id`) REFERENCES `Store` (`store_id`);
+
+--
+-- Constraints for table `Recipe`
+--
+ALTER TABLE `Recipe`
+  ADD CONSTRAINT `user_id_FOREIGN_KEY2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
