0d44324b2cc220391300c25a3c5f74a671793fbf
santiagu-gap <santiagu.gap@gmail.com>
2024-05-02 11:40:13 -0400

    Added debugging to web-scraper

--- app/scraping/metro.php
+++ app/scraping/metro.php
@@ -1,125 +1,76 @@
 <?php
-
 require 'vendor/autoload.php';
 
-use Symfony\Component\BrowserKit\HttpBrowser;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\DomCrawler\Crawler;
-
-function getRandomUserAgent()
-{
-    // Read the contents of the JSON file
-    $userAgentsJson = file_get_contents('user-agents_true.json');
-    
-    // Parse JSON data to an array
-    $userAgentsArray = json_decode($userAgentsJson, true);
-    
-    // Randomly select a user agent
-    $randomIndex = array_rand($userAgentsArray);
-    return $userAgentsArray[$randomIndex];
-}
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 function scrapeMetroItems($query)
 {
-    $browser = new HttpBrowser(HttpClient::create());
+    // Create an HttpClient instance
+    $client = HttpClient::create();
+
+    // Define the URL
     $address = 'https://www.metro.ca/en/online-grocery/search?filter=' . $query;
+
+    try {
+        // Send a GET request
+        $response = $client->request('GET', $address, [
+            'headers' => [
+                'User-Agent' => 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+                'Referer' => 'https://www.metro.ca/',
+            ],
+        ]);
+
+        // Get the status code directly from the response
+        $statusCode = $response->getStatusCode();
+        echo "Response Status Code: " . $statusCode . "\n";
+
+        // Check if the request was successful (status code 200)
+        if ($statusCode === 200) {
+            $content = $response->getContent();
+
+            // Parse the HTML content
+            $crawler = new Crawler($content);
+            $products = $crawler->filter('div.default-product-tile.tile-product.item-addToCart');
+
+            $items = [];
+            $products->each(function (Crawler $product) use (&$items) {
+                $item_id = $product->attr("data-product-code");
+                $name = $product->filter('div.head__title')->text();
+                $quantity = $product->filter('span.head__unit-details')->text();
+                $brand = $product->filter('span.head__brand')->text();
+                $price = $product->filter('span.price-update')->text();
+                $image = $product->filter('img[data-default="/images/shared/placeholders/icon-no-picture.svg"]')->attr('src');
+                $link = $product->filter('a.product-details-link')->attr('href');
+                $link = 'https://www.metro.ca' . $link;
     
-    // Get a random user agent
-    $randomUserAgent = getRandomUserAgent();
-
-    echo $randomUserAgent;
+                $item = [
+                    'item_id' => 'metro' . $item_id,
+                    'name' => $name,
+                    'quantity' => $quantity,
+                    'brand' => $brand,
+                    'price' => $price,
+                    'image' => $image,
+                    'link' => $link,
+                    'store' => "metro"
+                ];
     
-    $crawler = $browser->request('GET', $address, [
-        'headers' => [
-            'User-Agent' => $randomUserAgent, 
-            'Referer'=>'https://www.metro.ca/',
-        ],
-    ]);
+                $items[] = $item;
+            });
 
-    $crawler = new Crawler($crawler->html());
-    $products = $crawler->filter('div.default-product-tile.tile-product.item-addToCart');
-
-    $items = [];
-    $products->each(function (Crawler $product) use (&$items) {
-        $item_id = $product->attr("data-product-code");
-        $name = $product->filter('div.head__title')->text();
-        $quantity = $product->filter('span.head__unit-details')->text();
-        $brand = $product->filter('span.head__brand')->text();
-        $price = $product->filter('span.price-update')->text();
-        $image = $product->filter('img[data-default="/images/shared/placeholders/icon-no-picture.svg"]')->attr('src');
-        $link = $product->filter('a.product-details-link')->attr('href');
-        $link = 'https://www.metro.ca' . $link;
-
-        $item = [
-            'item_id' => 'metro' . $item_id,
-            'name' => $name,
-            'quantity' => $quantity,
-            'brand' => $brand,
-            'price' => $price,
-            'image' => $image,
-            'link' => $link,
-            'store' => "metro"
-        ];
-
-        $items[] = $item;
-    });
-
-    return $items;
+            print_r($items);
+            return $items;
+        } else {
+            echo "Error: Request failed with status code " . $statusCode . "\n";
+            return [];
+        }
+    } catch (TransportExceptionInterface $e) {
+        echo "Error: " . $e->getMessage() . "\n";
+        return [];
+    }
 }
 
-// scrapeMetroItems("eggs");
-
-
-// require 'vendor/autoload.php';
-
-// use Symfony\Component\BrowserKit\HttpBrowser;
-// use Symfony\Component\HttpClient\HttpClient;
-// use Symfony\Component\DomCrawler\Crawler;
-
-// function scrapeMetroItems($query)
-// {
-//         $browser = new HttpBrowser(HttpClient::create());
-//         $address = 'https://www.metro.ca/en/online-grocery/search?filter=' . $query;
-//         // print_r($address);
-//         $crawler = $browser->request('GET', $address, [
-//             'headers' => [
-//                 'User-Agent' => 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)', 
-//                 'Referer'=>'https://www.metro.ca/',
-//             ],
-//         ]);
-
-//         $crawler = new Crawler($crawler->html());
-//         $products = $crawler->filter('div.default-product-tile.tile-product.item-addToCart');
-
-//         $items = [];
-//         $products->each(function (Crawler $product) use (&$items) {
-//             $item_id = $product->attr("data-product-code");
-//             $name = $product->filter('div.head__title')->text();
-//             $quantity = $product->filter('span.head__unit-details')->text();
-//             $brand = $product->filter('span.head__brand')->text();
-//             $price = $product->filter('span.price-update')->text();
-//             $image = $product->filter('img[data-default="/images/shared/placeholders/icon-no-picture.svg"]')->attr('src');
-//             $link = $product->filter('a.product-details-link')->attr('href');
-//             $link = 'https://www.metro.ca' . $link;
-
-//             $item = [
-//                 'item_id' => 'metro' . $item_id,
-//                 'name' => $name,
-//                 'quantity' => $quantity,
-//                 'brand' => $brand,
-//                 'price' => $price,
-//                 'image' => $image,
-//                 'link' => $link,
-//                 'store' => "metro"
-//             ];
-
-//             $items[] = $item;
-//         });
-
-//     // print_r($items);
-//     return $items;
-// }
-
-// // scrapeMetroItems("eggs");
-
-// ?>
+scrapeMetroItems("eggs");
