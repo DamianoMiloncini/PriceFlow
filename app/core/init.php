37a393e2a5f05a462b0145c6ae953fc4b9e98424
santiagu-gap <santiagu.gap@gmail.com>
2024-05-09 10:38:59 -0400

    Merged the composer files

--- app/core/init.php
+++ app/core/init.php
@@ -3,9 +3,7 @@ session_start();
 require('app/core/App.php');
 require('app/core/Controller.php');
 require('app/core/autoload.php');
-//require('app/scraping/vendor/autoload.php');
-//require('vendor/autoload.php');
+require('vendor/autoload.php');
 //require('app/scraping/metro.php');
 // require('app/scraping/superc.php');
 
-require('vendor/autoload.php');
