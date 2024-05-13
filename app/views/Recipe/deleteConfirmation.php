2a09f56a76e1c95a2d84663f96eb62e9355aec0c
Sheldon Green <2076213@edu.vaniercollege.qc.ca>
2024-05-13 01:29:47 -0400

    Internationalization on deleteConfirmation view

--- app/views/Recipe/deleteConfirmation.php
+++ app/views/Recipe/deleteConfirmation.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Delete Confirmation</title>
+    <title><?=__('Delete Confirmation')?></title>
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -24,13 +24,13 @@
     </style>
 </head>
 <body>
-    <h1>Delete Confirmation</h1>
-    <p class="confirmation-message">Are you sure you want to delete the recipe "<?php echo htmlspecialchars($recipe['title']); ?>"?</p>
+    <h1><?=__('Delete Confirmation')?></h1>
+    <p class="confirmation-message"><?=__('Are you sure you want to delete the recipe')?> "<?php echo htmlspecialchars($recipe['title']); ?>"?</p>
     <form action="/Recipe/delete/<?php echo $recipe['recipe_id']; ?>" method="post">
-        <input class="confirm-btn" type="submit" name="confirm" value="Yes">
+        <input class="confirm-btn" type="submit" name="confirm" value="<?=__('Yes')?>">
     </form>
     <form action="/Recipe/displayAll" method="get">
-        <input class="cancel-btn" type="submit" name="cancel" value="Cancel">
+        <input class="cancel-btn" type="submit" name="cancel" value="<?=__('Cancel')?>">
     </form>
 </body>
 </html>
